35973e88bfb490234e0e3092008089e14ac958a9
Jorge <llanezarodriguezjorge@gmail.com>
2026-02-15 15:06:31 +0100

    Create ModBlocks class

--- src/main/java/com/github/uocraftteam/uocraft/Uocraft.java
+++ src/main/java/com/github/uocraftteam/uocraft/Uocraft.java
@@ -1,34 +1,11 @@
 package com.github.uocraftteam.uocraft;
 
+import com.github.uocraftteam.uocraft.block.ModBlocks;
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.material.MapColor;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
-import net.neoforged.neoforge.registries.DeferredRegister;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
@@ -45,5 +22,7 @@ public class Uocraft {
 
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+
+        ModBlocks.register(modEventBus);
     }
 }
--- src/main/java/com/github/uocraftteam/uocraft/block/ModBlocks.java
+++ src/main/java/com/github/uocraftteam/uocraft/block/ModBlocks.java
@@ -1,4 +1,29 @@
 package com.github.uocraftteam.uocraft.block;
 
+import com.github.uocraftteam.uocraft.Uocraft;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
 public class ModBlocks {
+    public static DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(Uocraft.MODID);
+
+    public static final DeferredBlock<Block> EII_BLOCK = BLOCKS.register("eii_block", registryName -> new Block(
+            BlockBehaviour.Properties.of()
+                    .setId(ResourceKey.create(Registries.BLOCK, registryName))
+                    .destroyTime(1.5f)
+                    .explosionResistance(6.0f)
+                    .sound(SoundType.STONE)
+                    .lightLevel(state -> 0)
+    ));
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
 }
