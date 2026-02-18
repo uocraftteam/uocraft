06eb3e92c1c4dd8a074ce2d92ac62404d4fb1a71
Jorge <llanezarodriguezjorge@gmail.com>
2026-02-18 19:55:02 +0100

    WIP coffee machine block

---
+++ src/main/java/com/github/uocraftteam/uocraft/block/custom/CoffeeMachineBlock.java
@@ -0,0 +1,90 @@
+package com.github.uocraftteam.uocraft.block.custom;
+
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.ScheduledTickAccess;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DoublePlantBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import org.jspecify.annotations.Nullable;
+
+public class CoffeeMachineBlock extends Block {
+    public CoffeeMachineBlock(Properties p_49795_) {
+        super(p_49795_);
+    }
+    /*
+    public static final EnumProperty<Direction> FACING = BlockStateProperties.HORIZONTAL_FACING;
+    public static final EnumProperty<DoubleBlockHalf> HALF =  BlockStateProperties.DOUBLE_BLOCK_HALF;
+    public static final IntegerProperty COFFEES_REMAINING = IntegerProperty.create("coffees_remaining", 0, 5);
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING, HALF, COFFEES_REMAINING);
+    }
+
+    @Override
+    public @Nullable BlockState getStateForPlacement(BlockPlaceContext context) {
+        Direction direction = context.getHorizontalDirection().getOpposite();
+
+        BlockPos pos = context.getClickedPos();
+        Level level = context.getLevel();
+
+        if (pos.getY() < level.getMaxY() - 1 && level.getBlockState(pos.above()).canBeReplaced()) {
+            return super.getStateForPlacement(context);
+        } else {
+            return null
+
+        }
+
+    }
+
+    @Override
+    public void setPlacedBy(Level level, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack stack) {
+        super.setPlacedBy(level, pos, state, placer, stack);
+    }
+
+    @Override
+    public BlockState playerWillDestroy(Level level, BlockPos pos, BlockState state, Player player) {
+        if (!level.isClientSide() && (player.isCreative() || !player.hasCorrectToolForDrops(state, level, pos))) {
+            DoubleBlockHalf half = state.getValue(HALF);
+            if (half == DoubleBlockHalf.UPPER) {
+                BlockPos blockpos = pos.below();
+                BlockState blockstate = level.getBlockState(blockpos);
+
+                // Check if the block below is the same block and is the LOWER half
+                if (blockstate.is(state.getBlock()) && blockstate.getValue(HALF) == DoubleBlockHalf.LOWER) {
+                    // Flag 35 = (1 | 2 | 32) -> Update neighbors, update clients, and PREVENT DROPS
+                    level.setBlock(blockpos, Blocks.AIR.defaultBlockState(), 35);
+                    // Optional: Triggers the block-break sound and particles
+                    level.levelEvent(player, 2001, blockpos, Block.getId(blockstate));
+                }
+            }
+        }
+        return super.playerWillDestroy(level, pos, state, player);
+    }
+
+
+
+    public CoffeeMachineBlock(Block.Properties properties) {
+        super(properties);
+        this.registerDefaultState(getStateDefinition().any()
+        .setValue(FACING, Direction.NORTH)
+                .setValue(HALF, DoubleBlockHalf.LOWER)
+                .setValue(COFFEES_REMAINING, 5);
+    }*/
+}
