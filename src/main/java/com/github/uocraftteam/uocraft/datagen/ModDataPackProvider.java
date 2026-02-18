b660bd8b6c6fedfacf7b10b5236f4cbbe76533d3
Jorge <llanezarodriguezjorge@gmail.com>
2026-02-18 19:53:03 +0100

    add dataPack Provider

---
+++ src/main/java/com/github/uocraftteam/uocraft/datagen/ModDataPackProvider.java
@@ -0,0 +1,17 @@
+package com.github.uocraftteam.uocraft.datagen;
+
+import com.github.uocraftteam.uocraft.Uocraft;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
+
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
+public class ModDataPackProvider extends DatapackBuiltinEntriesProvider {
+
+    public ModDataPackProvider(PackOutput output, CompletableFuture<RegistrySetBuilder.PatchedRegistries> registries, Set<String> modIds) {
+        super(output, registries, Set.of(Uocraft.MODID));
+    }
+}
