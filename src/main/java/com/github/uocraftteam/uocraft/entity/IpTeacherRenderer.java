da13fc6d09f15039b8549e8c215d291d70e3a14d
Jorge <llanezarodriguezjorge@gmail.com>
2026-02-21 19:43:43 +0100

    refactor: clean imports

--- src/main/java/com/github/uocraftteam/uocraft/entity/IpTeacherRenderer.java
+++ src/main/java/com/github/uocraftteam/uocraft/entity/IpTeacherRenderer.java
@@ -2,15 +2,12 @@ package com.github.uocraftteam.uocraft.entity;
 
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelLayers;
-import net.minecraft.client.model.geom.ModelPart;
-import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.HumanoidMobRenderer;
 import net.minecraft.client.renderer.entity.state.EntityRenderState;
 import net.minecraft.client.renderer.entity.state.HumanoidRenderState;
 import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
 import net.minecraft.resources.Identifier;
-import net.neoforged.neoforge.client.model.item.DynamicFluidContainerModel;
 
 public class IpTeacherRenderer extends HumanoidMobRenderer {
     public IpTeacherRenderer(EntityRendererProvider.Context context, HumanoidModel model, float shadowRadius) {
